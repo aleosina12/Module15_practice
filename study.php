5dd4ed1a63fb8c573e5745ad6cb008b352bba821
aleosina12 <68166171+aleosina12@users.noreply.github.com>
2020-10-11 12:03:04 +0300

    Add files via upload

---
+++ study.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Процесс моего обучения </title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+
+<div class="flex-container">
+
+    <div class="header">
+        <?php include 'logo.inc.php' ?>
+        <?php include 'menu.inc.php' ?>
+    </div>
+
+
+    
+
+
+        <div class="article">
+            <p class="text">
+                В модуле PHP я изучил основы языка программирования PHP. Ознакомился различием между статическим и динамическим сайтом, для чего нужен язык PHP, синтаксисом. Научился разворачивать локальный сервис и осуществил вывод данных.
+                По окончании изучения я смог самостоятельно применить полученные знания на практике, осуществить вывод данных на страницу.
+            </p>
+        </div>
+    </div>
+
+    <?php include 'footer.inc.php' ?>
+
+</div>
+
+
+</body>
+</html>
